5ac37c71f8b28a42a070c8a420db43986cb99526
Muhammad Ikhsan <iksan8991@gmail.com>
2023-09-18 13:59:25 +0800

    Update connection.js

--- API-Kalla/connection.js
+++ API-Kalla/connection.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USERNAME, 
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+    // host: process.env.DB_HOST, 
+    // user: process.env.DB_USERNAME, 
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DBNAME,
+    // waitForConnections: true,
+    // connectionLimit: 10,
+    // queueLimit: 0
+     host:"bnwraqawhrfuvvpy2tpx-mysql.services.clever-cloud.com",
+    user: "uyozibhdvgm12nde",
+    password:"T5v27I3QczmffcKGXhk7",
+    database:"bnwraqawhrfuvvpy2tpx"
    
 });
 
