53d280bea9462e031aebfea3ab8b96a62b029fb6
Muhammad Ikhsan <iksan8991@gmail.com>
2023-09-18 14:24:32 +0800

    update

--- API-Kalla/connection.js
+++ API-Kalla/connection.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
+
+    
     host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
