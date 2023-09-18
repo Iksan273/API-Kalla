013b2a667d40bd119949f88962217cb04b8390ab
Muhammad Ikhsan <iksan8991@gmail.com>
2023-09-18 16:49:01 +0800

    deploy vercel

--- API-Kalla/connection.js
+++ API-Kalla/connection.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 
 // Konfigurasi koneksi ke database
 const dbConfig = {
-  // host: 'bnwraqawhrfuvvpy2tpx-mysql.services.clever-cloud.com',
-  // user: 'uyozibhdvgm12nde', 
-  // password: 'EFUIQF2Dj1x0yc99cYM9', 
-  // database: 'bnwraqawhrfuvvpy2tpx' 
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME, 
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_DBNAME 
+  host: 'bnwraqawhrfuvvpy2tpx-mysql.services.clever-cloud.com',
+  user: 'uyozibhdvgm12nde', 
+  password: 'EFUIQF2Dj1x0yc99cYM9', 
+  database: 'bnwraqawhrfuvvpy2tpx' 
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USERNAME, 
+  // password: process.env.DB_PASSWORD, 
+  // database: process.env.DB_DBNAME 
 };
 
 // Membuat koneksi ke database
