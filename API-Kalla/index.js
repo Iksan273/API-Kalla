8a45c6feb5e3f76b265e544c701acb7503f10081
Muhammad Ikhsan <iksan8991@gmail.com>
2023-09-18 10:06:19 +0800

    Update index.js

--- API-Kalla/index.js
+++ API-Kalla/index.js
@@ -61,15 +61,15 @@ app.post('/register', (req, res) => {
   // Simpan pengguna yang belum diverifikasi dalam array
   unverifiedUsers.push({ firstName, lastName, email, username, password });
 
-  transporter.sendMail(mailOptions, (error, info) => {
+  transporter.sendMail(mailOptions, async (error, info) => {
     if (error) {
       console.error(error);
       res.status(500).json({ message: 'Gagal mengirim email verifikasi' });
     } else {
       console.log('Email verifikasi terkirim: ' + info.response);
       const sql = "INSERT INTO user (firstName, lastName, email, username, password) VALUES (?, ?, ?, ?, ?)";
-      
-      db.query(sql, [firstName, lastName, email, username, bcrypt.hash(password, 10)], (error, result) => {
+      const hashedPassword = await bcrypt.hash('password', 10);
+      db.query(sql, [firstName, lastName, email, username, hashedPassword], (error, result) => {
         if (error) {
           console.error('Error executing SQL query:', error);
           return res.status(500).json({ message: 'Internal Server Error' });
