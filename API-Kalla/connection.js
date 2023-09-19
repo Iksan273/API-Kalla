abd67e02ae4b9d37910a5ddb07b5ddea7c266855
Muhammad Ikhsan <iksan8991@gmail.com>
2023-09-19 10:30:11 +0800

    Update connection.js

--- API-Kalla/connection.js
+++ API-Kalla/connection.js
@@ -1,19 +1,27 @@
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
-// Konfigurasi pool koneksi ke database
-const pool = mysql.createPool({
-  // host: 'bnwraqawhrfuvvpy2tpx-mysql.services.clever-cloud.com',
-  // user: 'uyozibhdvgm12nde', 
-  // password: 'EFUIQF2Dj1x0yc99cYM9', 
-  // database: 'bnwraqawhrfuvvpy2tpx' 
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME, 
-  password: process.env.DB_PASSWORD, 
-  database: process.env.DB_DBNAME,
-  connectionLimit: 10, // Jumlah maksimum koneksi dalam pool
+// Konfigurasi koneksi ke database
+const dbConfig = {
+  host: 'bnwraqawhrfuvvpy2tpx-mysql.services.clever-cloud.com',
+  user: 'uyozibhdvgm12nde', 
+  password: 'EFUIQF2Dj1x0yc99cYM9', 
+  database: 'bnwraqawhrfuvvpy2tpx' 
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USERNAME, 
+  // password: process.env.DB_PASSWORD, 
+  // database: process.env.DB_DBNAME 
+};
+
+// Membuat koneksi ke database
+const connection = mysql.createConnection(dbConfig);
+
+// Membuka koneksi ke database
+connection.connect((err) => {
+  if (err) {
+    console.error('Koneksi ke database gagal:', err);
+  } else {
+    console.log('Terhubung ke database MySQL');
+  }
 });
 
-// Membuat pool koneksi
-const poolPromise = pool.promise();
-
-module.exports = poolPromise;
+module.exports = connection;
