403532b0f73dcf2b10fc313a7625a21113856ae8
Muhammad Ikhsan <iksan8991@gmail.com>
2023-10-05 10:31:15 +0800

    update profile image

--- API-Kalla/index.js
+++ API-Kalla/index.js
@@ -191,6 +191,31 @@ app.put('/profile/:userId', async (req, res) => {
   });
 });
 
+app.put('/profileImage/:userId', async (req, res) => {
+  const userId = req.params.userId; // Mendapatkan userId dari URL
+  const {url } = req.body;
+
+  // Validasi data
+  if (!url) {
+    return res.status(400).json({ message: 'Semua data harus terisi' });
+  }
+ 
+  const updateQuery = "UPDATE user SET urlImage = ? WHERE id = ?";
+  db.query(updateQuery, [url, userId], (error, result) => {
+    if (error) {
+      console.error('Error executing SQL query:', error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+
+    if (result.affectedRows === 0) {
+      return res.status(404).json({  error: true,message: 'Pengguna tidak ditemukan' });
+    }
+
+    res.status(200).json({  error: false,message: 'Foto Profile Berhasil di update' });
+  });
+});
+
+
 
 
 
